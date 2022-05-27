082632024e734085cb2894024cd06df7912521be
Anti-puff-dev <fmantipuff@gmail.com>
2022-05-26 22:11:05 -0300

    Spec added

---
+++ Spec/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spec
+{
+    class Program
+    {
+        static int thread_count = 100;
+        static ThreadsManager.Functions tm = new ThreadsManager.Functions(thread_count);
+
+        static List<string> list = new List<string>();
+
+
+        static void Main(string[] args)
+        {
+            Test1();
+            Console.ReadKey();
+        }
+
+
+
+
+        static void Test1()
+        {
+            Console.WriteLine("Test1");
+            //Populate list
+            for (int i=0; i<1000; i++)
+            {
+                list.Add(i.ToString());
+            }
+
+            new Task(async() => await Init()).Start();
+        }
+
+
+        static async Task Init()
+        {
+            Console.WriteLine("init " + list.Count());
+            if (list.Count() == 0)
+            {
+                Thread.Sleep(250);
+                await Init();
+            }
+            else
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    tm.AddFunction(Proc, "");
+                }
+            }
+        }
+
+
+        static bool Proc(string args)
+        {
+            string _queue = null;
+
+            try
+            {
+                if (list.Count() == 0)
+                {
+                    Thread.Sleep(250);
+                    new Task(async () => await Init()).Start();
+                }
+                else
+                {
+                    lock (list)
+                    {
+                        _queue = list[0];
+                        list.RemoveAt(0);
+                    }
+
+                    if (_queue != null)
+                    {
+                        Int32 a = Convert.ToInt32(_queue) * 2;
+                        Console.WriteLine(a.ToString());
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ThreadsManager/Functions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadsManager
+{
+    public struct FN
+    {
+        public Func<string, bool> func;
+        public String args;
+    }
+
+    public struct FNObj
+    {
+        public Func<Object, bool> func;
+        public Object args;
+    }
+
+    public class Functions
+    {
+        public delegate void ThreadDelegate();
+
+        public Functions(int p)
+        {
+            ThreadPool.SetMaxThreads(p, p);
+        }
+
+        public void AddFunction(Func<bool> MyFunc, Func<bool> CallBack)
+        {
+            Func<bool>[] fn = new Func<bool>[2];
+            fn[0] = MyFunc;
+            fn[1] = CallBack;
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ProcessThread), fn);
+        }
+
+        public void AddFunction(Func<string, bool> MyFunc, String args)
+        {
+            FN fn = new FN();
+            fn.func = MyFunc;
+            fn.args = args;
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ProcessThreadArgs), fn);
+        }
+
+
+        public void AddFunction(Func<Object, bool> MyFunc, Object args)
+        {
+            FNObj fn = new FNObj();
+            fn.func = MyFunc;
+            fn.args = args;
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ProcessThreadObj), fn);
+        }
+
+
+        private void ProcessThread(object a)
+        {
+            Func<bool>[] fn = (Func<bool>[])a;
+            fn[0]();
+            fn[1]();
+        }
+
+        private void ProcessThreadArgs(object a)
+        {
+            FN fn = (FN)a;
+            fn.func(fn.args);
+        }
+
+        private void ProcessThreadObj(object a)
+        {
+            FNObj fn = (FNObj)a;
+            fn.func(fn.args);
+        }
+    }
+}
